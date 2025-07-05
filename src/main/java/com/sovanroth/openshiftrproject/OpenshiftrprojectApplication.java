14429ffd84ab2ca8c25c3c5134f5a8a8bcb3d168
Sovanroth13 <sovanroth016@gmail.com>
2025-07-05 22:24:19 +0700

    first commit

---
+++ src/main/java/com/sovanroth/openshiftrproject/OpenshiftrprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sovanroth.openshiftrproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftrprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftrprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sovanroth/openshiftrproject/OpenshiftrprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sovanroth.openshiftrproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenshiftrprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
